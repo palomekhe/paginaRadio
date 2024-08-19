e0f101785b2ba87832b9bb4f8e3567d034690df4
palomekhe <152027302+palomekhe@users.noreply.github.com>
2024-08-19 11:33:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openDrawer() {
+    document.getElementById("menuDrawer").style.width = "250px";
+    document.getElementById("mainContent").style.marginLeft = "250px";
+}
+
+function closeDrawer() {
+    document.getElementById("menuDrawer").style.width = "0";
+    document.getElementById("mainContent").style.marginLeft = "0";
+}
